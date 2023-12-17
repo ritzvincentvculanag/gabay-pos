4461ebe8bd74b660562418d308978676eb1a0372
Ritchie <ritchieculanag@gmail.com>
2023-12-17 22:35:43 +0800

    update: add initial implementation of Transaction

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/fragments/Transaction.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/fragments/Transaction.java
@@ -3,64 +3,43 @@ package me.jhayzonalbay.rmmcgabay.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import me.jhayzonalbay.rmmcgabay.R;
+import me.jhayzonalbay.rmmcgabay.utils.Widget;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Transaction#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Transaction extends Fragment {
+public class Transaction extends Fragment implements Widget {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Widgets
+    private TextInputLayout search;
 
-    public Transaction() {
-        // Required empty public constructor
-    }
+    private Button cart;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Transaction.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Transaction newInstance(String param1, String param2) {
-        Transaction fragment = new Transaction();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private RecyclerView products;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_transaction, container, false);
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void initWidgets() {
+        search = view.findViewById(R.id.til_trans_search);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction, container, false);
+        cart = view.findViewById(R.id.btn_trans_cart);
+
+        products =view.findViewById(R.id.rv_trans_products);
     }
 }
