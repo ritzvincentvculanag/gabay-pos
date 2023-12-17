044f652ef22d3cba7b6dbec1173ca359ae148930
Ritchie <ritchieculanag@gmail.com>
2023-12-17 22:38:10 +0800

    update: add initial Product model

---
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/models/Product.java
@@ -0,0 +1,62 @@
+package me.jhayzonalbay.rmmcgabay.models;
+
+public class Product {
+
+    private Integer id;
+
+    private String name;
+    private String description;
+    private Double price;
+
+    public Product() {
+
+    }
+
+    public Product(String name, String description, Double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
