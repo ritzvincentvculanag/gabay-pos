82d992dbf04d89be7623b00df0f2093347a5f698
Ritchie <ritchieculanag@gmail.com>
2023-12-17 23:19:14 +0800

    update: add dependency for barcode scanner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:$nav_version")
     implementation("androidx.navigation:navigation-ui:$nav_version")
 
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
