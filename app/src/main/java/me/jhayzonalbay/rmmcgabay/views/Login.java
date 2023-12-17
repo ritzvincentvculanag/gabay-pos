b90cb08473789a8c4f7f9f64b20630babf72b78c
Ritchie <ritchieculanag@gmail.com>
2023-12-17 13:35:01 +0800

    update: add initial Login

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Login.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Login.java
@@ -3,14 +3,34 @@ package me.jhayzonalbay.rmmcgabay.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import me.jhayzonalbay.rmmcgabay.R;
+import me.jhayzonalbay.rmmcgabay.utils.Widget;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements Widget {
+
+    // Widgets
+    private TextInputLayout username;
+    private TextInputLayout password;
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initWidgets();
+    }
+
+    @Override
+    public void initWidgets() {
+        username = findViewById(R.id.til_login_username);
+        password = findViewById(R.id.til_login_password);
+
+        login = findViewById(R.id.btn_login_signin);
     }
 }
