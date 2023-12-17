657793cee51f854de22bac7ccc0490f8c5403ef9
Ritchie <ritchieculanag@gmail.com>
2023-12-17 21:59:09 +0800

    update: setup initial hero activity

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Hero.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Hero.java
@@ -1,16 +1,33 @@
 package me.jhayzonalbay.rmmcgabay.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import me.jhayzonalbay.rmmcgabay.R;
+import me.jhayzonalbay.rmmcgabay.utils.Widget;
 
 public class Hero extends AppCompatActivity {
+
+    // Widgets
+    private BottomNavigationView navigation;
+    private NavHostFragment content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero);
+
+        navigation = findViewById(R.id.bnv_hero);
+        content = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fcv_hero);
+
+        if (content != null) {
+            NavigationUI.setupWithNavController(navigation, content.getNavController());
+        }
     }
+
 }
