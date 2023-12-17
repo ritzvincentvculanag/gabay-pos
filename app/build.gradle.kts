a4151fe89b4de8a32f8e79aa002aa1d6f3db0aa0
Ritchie <ritchieculanag@gmail.com>
2023-12-17 13:41:07 +0800

    update: add navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,10 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.6"
+
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
