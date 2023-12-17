0c9b56b564baa90c10bde1eae31e83102462be79
Ritchie <ritchieculanag@gmail.com>
2023-12-17 13:23:29 +0800

    update: rename MainActivity to Login

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Login.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Login.java
@@ -1,14 +1,16 @@
-package me.jhayzonalbay.rmmcgabay;
+package me.jhayzonalbay.rmmcgabay.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import me.jhayzonalbay.rmmcgabay.R;
+
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
