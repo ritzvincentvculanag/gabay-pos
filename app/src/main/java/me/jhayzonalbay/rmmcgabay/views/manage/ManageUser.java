5426c41c752798a37a86e2c25379ec5b0ce036d7
Ritchie <ritchieculanag@gmail.com>
2023-12-19 00:38:14 +0800

    update: add initial manage users

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/views/manage/ManageUser.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/manage/ManageUser.java
@@ -1,5 +1,7 @@
 package me.jhayzonalbay.rmmcgabay.views.manage;
 
+import android.content.Intent;
+import android.hardware.lights.LightState;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,59 +10,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import me.jhayzonalbay.rmmcgabay.R;
+import me.jhayzonalbay.rmmcgabay.models.User;
+import me.jhayzonalbay.rmmcgabay.repositories.UserRepository;
+import me.jhayzonalbay.rmmcgabay.utils.Item;
+import me.jhayzonalbay.rmmcgabay.views.edit.EditUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ManageUser#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ManageUser extends Fragment {
+public class ManageUser extends Fragment implements Item {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<User> users;
 
-    public ManageUser() {
-        // Required empty public constructor
-    }
+    private UserRepository userRepository;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ManageUser.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ManageUser newInstance(String param1, String param2) {
-        ManageUser fragment = new ManageUser();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_manage_user, container, false);
+
+        userRepository = new UserRepository(getContext());
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void edit(int position) {
+        User user = users.get(position);
+        Intent goToEditUser = new Intent(getContext(), EditUser.class);
+        goToEditUser.putExtra("EXT_USER", user);
+        startActivity(goToEditUser);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manage_user, container, false);
+    public void delete(int position) {
+        User user = users.get(position);
+        userRepository.delete(user);
+        users.remove(position);
     }
 }
