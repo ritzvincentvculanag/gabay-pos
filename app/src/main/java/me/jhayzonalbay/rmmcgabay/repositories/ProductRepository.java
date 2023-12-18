633dcef7ae1eb4d536e311898f64a84dc177ccea
Ritchie <ritchieculanag@gmail.com>
2023-12-18 19:46:37 +0800

    update: add initial Product repository

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/repositories/ProductRepository.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/repositories/ProductRepository.java
@@ -1,48 +1,123 @@
 package me.jhayzonalbay.rmmcgabay.repositories;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import me.jhayzonalbay.rmmcgabay.db.Database;
+import me.jhayzonalbay.rmmcgabay.models.Category;
 import me.jhayzonalbay.rmmcgabay.models.Product;
 
 public class ProductRepository implements CrudRepository<Product> {
 
-    private List<Product> products;
+    private Context context;
 
-    public ProductRepository() {
-        products = new ArrayList<>();
-    }
-
-    public ProductRepository(List<Product> products) {
-        this.products = products;
+    public ProductRepository(Context context) {
+        this.context = context;
     }
 
     @Override
     public Product insert(Product product) {
-        products.add(product);
+        SQLiteDatabase db = Database.getWritableDatabase(context);
+        ContentValues values = new ContentValues();
+
+        values.put(Product.NAME, product.getName());
+        values.put(Product.DESCRIPTION, product.getDescription());
+        values.put(Product.BARCODE, product.getBarcode());
+        values.put(Product.PRICE, product.getPrice());
+        values.put(Product.CATEGORY, product.getCategory().getId());
+
+        long productId = db.insert("Product", null, values);
+        product.setId((int) productId);
+
+        Toast.makeText(context, "Product added!", Toast.LENGTH_SHORT).show();
+
         return product;
     }
 
     @Override
     public Product update(Product product) {
-        for (Product item : products) {
-            if (item.getName().equals(product.getName())) {
-                item = product;
-                return item;
-            }
-        }
+        SQLiteDatabase db = Database.getWritableDatabase(context);
+        ContentValues values = new ContentValues();
+        String selection = Product.ID + " LIKE ?";
+        String[] selectionArgs = { String.valueOf(product.getId()) };
 
-        return null;
+        values.put(Product.NAME, product.getName());
+        values.put(Product.DESCRIPTION, product.getDescription());
+        values.put(Product.BARCODE, product.getBarcode());
+        values.put(Product.PRICE, product.getPrice());
+        values.put(Product.CATEGORY, product.getCategory().getId());
+
+        MaterialAlertDialogBuilder alert = new MaterialAlertDialogBuilder(context);
+
+        alert.setTitle("Update product");
+        alert.setMessage("Are you sure you want to update this product?");
+        alert.setPositiveButton("Yes", ((dialog, which) -> {
+            db.update("Category", values, selection, selectionArgs);
+            Toast.makeText(context, product.getName() + " updated!", Toast.LENGTH_SHORT).show();
+        }));
+        alert.setNegativeButton("No", ((dialog, which) -> Toast.makeText(context, "Aborting", Toast.LENGTH_SHORT).show()));
+        alert.show();
+
+        return product;
     }
 
     @Override
     public Product delete(Product product) {
-        products.remove(product);
+        SQLiteDatabase db = Database.getWritableDatabase(context);
+        String selection = Product.ID + " LIKE ?";
+        String[] selectionArgs = { String.valueOf(product.getId()) };
+
+        MaterialAlertDialogBuilder alert = new MaterialAlertDialogBuilder(context);
+
+        alert.setTitle("Delete product");
+        alert.setMessage("Are you sure you want to delete this product?");
+        alert.setPositiveButton("Yes", ((dialog, which) -> {
+            db.delete("Category", selection, selectionArgs);
+            Toast.makeText(context, product.getName() + " deleted!", Toast.LENGTH_SHORT).show();
+        }));
+        alert.setNegativeButton("No", ((dialog, which) -> Toast.makeText(context, "Aborting", Toast.LENGTH_SHORT).show()));
+        alert.show();
+
         return product;
     }
 
     @Override
     public List<Product> getAll() {
+        CategoryRepository categoryRepository = new CategoryRepository(context);
+        List<Product> products = new ArrayList<>();
+        SQLiteDatabase db = Database.getReadableDatabase(context);
+        Cursor cursor = db.query(
+                "Product",
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        while (cursor.moveToNext()) {
+            long id = cursor.getLong(cursor.getColumnIndexOrThrow(Product.ID));
+            long categoryId = cursor.getLong(cursor.getColumnIndexOrThrow(Product.CATEGORY));
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(Product.NAME));
+            String description = cursor.getString(cursor.getColumnIndexOrThrow(Product.DESCRIPTION));
+            String barcode = cursor.getString(cursor.getColumnIndexOrThrow(Product.BARCODE));
+            double price = cursor.getDouble(cursor.getColumnIndexOrThrow(Product.PRICE));
+
+
+            Category category = categoryRepository.getCategory((int) categoryId);
+            Product product = new Product(name, barcode, price, category);
+            products.add(product);
+        }
+
         return products;
     }
 }
