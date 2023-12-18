0d3140a38e6993b4ca16a0e66d6d059937913bea
Ritchie <ritchieculanag@gmail.com>
2023-12-18 15:46:16 +0800

    update: add initial PurchasedItem model

---
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/models/PurchasedItem.java
@@ -0,0 +1,8 @@
+package me.jhayzonalbay.rmmcgabay.models;
+
+public class PurchasedItem {
+
+    public static final String PRODUCT_ID = "product_id";
+    public static final String TRANSACTION_ID = "transaction_id";
+
+}
