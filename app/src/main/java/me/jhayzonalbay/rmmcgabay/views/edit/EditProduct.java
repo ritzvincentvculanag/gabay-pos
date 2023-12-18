378cf4612bb1a5fee4a4ef2fc3fc29af105ba1a5
Ritchie <ritchieculanag@gmail.com>
2023-12-18 16:17:38 +0800

    update: add initial activities for CRUD operation

---
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/edit/EditProduct.java
@@ -0,0 +1,16 @@
+package me.jhayzonalbay.rmmcgabay.views.edit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.jhayzonalbay.rmmcgabay.R;
+
+public class EditProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
+}
