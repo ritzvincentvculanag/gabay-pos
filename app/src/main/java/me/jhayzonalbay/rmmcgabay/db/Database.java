c4ccc0425addc15917ea9c7ac4fb716387f1ddfc
Ritchie <ritchieculanag@gmail.com>
2023-12-18 15:08:48 +0800

    update: add initial Database

---
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/db/Database.java
@@ -0,0 +1,56 @@
+package me.jhayzonalbay.rmmcgabay.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "Gabay";
+    private static final int DB_VERSION = 1;
+    private static final SQLiteDatabase.CursorFactory DB_FACTORY = null;
+    private static final Object MONITOR = new Object();
+
+    public static volatile Database instance = null;
+
+    public Database(@Nullable Context context) {
+        super(context, DB_NAME, DB_FACTORY, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public static SQLiteDatabase getWritableDatabase(Context context) {
+        if (instance == null) {
+            synchronized (MONITOR) {
+                if (instance == null) {
+                    instance = new Database(context);
+                }
+            }
+        }
+
+        return instance.getWritableDatabase();
+    }
+
+    public static SQLiteDatabase getReadableDatabase(Context context) {
+        if (instance == null) {
+            synchronized (MONITOR) {
+                if (instance == null) {
+                    instance = new Database(context);
+                }
+            }
+        }
+
+        return instance.getReadableDatabase();
+    }
+
+}
