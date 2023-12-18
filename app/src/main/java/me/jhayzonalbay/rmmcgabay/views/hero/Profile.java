5e3eb242705bdc331087de0601c464ff457a6baa
Ritchie <ritchieculanag@gmail.com>
2023-12-18 16:55:58 +0800

    update: add initial implementation of Profile fragment

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/views/hero/Profile.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/hero/Profile.java
@@ -1,5 +1,6 @@
 package me.jhayzonalbay.rmmcgabay.views.hero;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import me.jhayzonalbay.rmmcgabay.R;
+import me.jhayzonalbay.rmmcgabay.utils.Widget;
+import me.jhayzonalbay.rmmcgabay.views.Manage;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Profile#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Profile extends Fragment {
+public class Profile extends Fragment implements Widget {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button manageSystem;
 
-    public Profile() {
-        // Required empty public constructor
-    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Profile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Profile newInstance(String param1, String param2) {
-        Profile fragment = new Profile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        initWidgets();
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void initWidgets() {
+        manageSystem = view.findViewById(R.id.btn_profile_manage);
+        manageSystem.setOnClickListener(this::manageSystemAction);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    private void manageSystemAction(View view) {
+        Intent goToManage = new Intent(getContext(), Manage.class);
+        startActivity(goToManage);
     }
 }
