c298f0298a6ab798b638dc977bece5115ac80cc1
Ritchie <ritchieculanag@gmail.com>
2023-12-18 14:43:59 +0800

    update: add Category model

---
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/models/Category.java
@@ -0,0 +1,65 @@
+package me.jhayzonalbay.rmmcgabay.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Category implements Parcelable {
+
+    private int id;
+
+    private String name;
+
+    public Category() {
+
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    protected Category(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+    }
+
+    public static final Creator<Category> CREATOR = new Creator<Category>() {
+        @Override
+        public Category createFromParcel(Parcel in) {
+            return new Category(in);
+        }
+
+        @Override
+        public Category[] newArray(int size) {
+            return new Category[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+    }
+}
