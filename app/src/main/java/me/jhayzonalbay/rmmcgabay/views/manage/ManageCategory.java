1803ffdd7002c409d975456d4704457b7145539a
Ritchie <ritchieculanag@gmail.com>
2023-12-18 18:01:56 +0800

    update: render categories

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/views/manage/ManageCategory.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/manage/ManageCategory.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +13,12 @@ import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 import me.jhayzonalbay.rmmcgabay.R;
+import me.jhayzonalbay.rmmcgabay.models.Category;
+import me.jhayzonalbay.rmmcgabay.models.adapter.CategoryAdapter;
+import me.jhayzonalbay.rmmcgabay.repositories.CategoryRepository;
 import me.jhayzonalbay.rmmcgabay.views.edit.EditCategory;
 
 public class ManageCategory extends Fragment {
@@ -19,6 +26,11 @@ public class ManageCategory extends Fragment {
     private View view;
 
     private FloatingActionButton addCategory;
+
+    private List<Category> categories;
+    private RecyclerView categoriesView;
+    private CategoryAdapter categoryAdapter;
+    private CategoryRepository repository;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +43,14 @@ public class ManageCategory extends Fragment {
             startActivity(goToEditCategory);
         });
 
+        repository = new CategoryRepository(getContext());
+        categories = repository.getAll();
+        categoryAdapter = new CategoryAdapter(categories);
+        categoriesView = view.findViewById(R.id.rv_manage_category);
+        categoriesView.setAdapter(categoryAdapter);
+        categoriesView.setLayoutManager(new LinearLayoutManager(getContext()));
+
         return view;
     }
+
 }
