95bce53e2eb4a9d90c20fba8c1e54e8b9cca066a
Ritchie <ritchieculanag@gmail.com>
2023-12-18 14:49:49 +0800

    update: add UserType model

---
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/models/UserType.java
@@ -0,0 +1,65 @@
+package me.jhayzonalbay.rmmcgabay.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class UserType implements Parcelable {
+
+    private int id;
+
+    private String type;
+
+    public UserType() {
+
+    }
+
+    public UserType(String type) {
+        this.type = type;
+    }
+
+    protected UserType(Parcel in) {
+        id = in.readInt();
+        type = in.readString();
+    }
+
+    public static final Creator<UserType> CREATOR = new Creator<UserType>() {
+        @Override
+        public UserType createFromParcel(Parcel in) {
+            return new UserType(in);
+        }
+
+        @Override
+        public UserType[] newArray(int size) {
+            return new UserType[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(type);
+    }
+}
