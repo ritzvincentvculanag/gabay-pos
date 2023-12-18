8e64597401c16500a673a6860f0c3f6614c74bbf
Ritchie <ritchieculanag@gmail.com>
2023-12-18 17:13:22 +0800

    update: implement navigation between fragments

--- app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Manage.java
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Manage.java
@@ -1,16 +1,30 @@
 package me.jhayzonalbay.rmmcgabay.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import me.jhayzonalbay.rmmcgabay.R;
 
 public class Manage extends AppCompatActivity {
 
+    private NavHostFragment content;
+    private BottomNavigationView navigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage);
+
+        navigation = findViewById(R.id.bnv_manage);
+        content = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fcv_manage);
+
+        if (content != null) {
+            NavigationUI.setupWithNavController(navigation, content.getNavController());
+        }
     }
 }
