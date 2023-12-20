bed2fb967852797292a509e3b034231b80508ed0
Ritchie <ritchieculanag@gmail.com>
2023-12-20 23:44:40 +0800

    update: add initial Account activity

---
+++ app/src/main/java/me/jhayzonalbay/rmmcgabay/views/Account.java
@@ -0,0 +1,16 @@
+package me.jhayzonalbay.rmmcgabay.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.jhayzonalbay.rmmcgabay.R;
+
+public class Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
